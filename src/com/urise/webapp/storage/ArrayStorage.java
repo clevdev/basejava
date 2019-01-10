e01ac215d335af31e9464ebaf8a30d4cd815c2d5
clevdev <gusev.dev@gmail.com>
2019-01-10 23:50:34 +0300

    HW2:  - renamed class members

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,65 +4,53 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
     private static final int NOT_FOUND = -1;
-
-    private int getResumePosition(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return NOT_FOUND;
-    }
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void save(Resume r) {
-        if (getResumePosition(r.getUuid()) == NOT_FOUND) {
+    public void save(Resume resume) {
+        if (getIndex(resume.getUuid()) == NOT_FOUND) {
             if (size < storage.length) {
-                storage[size] = r;
+                storage[size] = resume;
                 size++;
             } else {
                 System.out.println("ERROR: Не возможно сохранить новый элемент. Переполнение");
             }
         } else {
 
-            System.out.println("ERROR: Resume c uuid=" + r.getUuid() + " уже существует");
+            System.out.println("ERROR: Resume c uuid=" + resume.getUuid() + " уже существует");
         }
 
     }
 
-    public void update(Resume r) {
-        int position = getResumePosition(r.getUuid());
-        if (position != NOT_FOUND) {
-            storage[position] = r;
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index != NOT_FOUND) {
+            storage[index] = resume;
         } else {
-            System.out.println("ERROR: Обновление не выполнено. Resume c uuid=" + r.getUuid() + " не существует");
+            System.out.println("ERROR: Обновление не выполнено. Resume c uuid=" + resume.getUuid() + " не существует");
         }
     }
 
     public Resume get(String uuid) {
-        int position = getResumePosition(uuid);
-        if (position != NOT_FOUND) {
-            return storage[position];
+        int index = getIndex(uuid);
+        if (index != NOT_FOUND) {
+            return storage[index];
         }
         System.out.println("ERROR: Resume c uuid=" + uuid + " не существует");
         return null;
     }
 
     public void delete(String uuid) {
-        int position = getResumePosition(uuid);
-        if (position != NOT_FOUND) {
-            storage[position] = storage[size - 1];
+        int index = getIndex(uuid);
+        if (index != NOT_FOUND) {
+            storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
         }
@@ -70,21 +58,23 @@ public class ArrayStorage {
 
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
 
-        if (size > 0) {
-            Resume resume[] = new Resume[size];
-            System.arraycopy(storage, 0, resume, 0, size);
-            return resume;
-        }
-
-        return new Resume[0];
+        Resume[] resume = new Resume[size];
+        System.arraycopy(storage, 0, resume, 0, size);
+        return resume;
     }
 
     public int size() {
         return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 }
