ee9a05fc34d5b268a397139fa6161cbb6cf1ac52
clevdev <gusev.dev@gmail.com>
2019-04-13 01:56:14 +0300

    HW4. final

---
+++ src/com/urise/webapp/MainReflection.java
@@ -0,0 +1,26 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume,"new_uuid");
+        System.out.println(resume);
+
+        //invoke toString
+
+        Method method = resume.getClass().getDeclaredMethod("toString",null);
+        System.out.println(method.getName());
+        System.out.println(method.invoke(resume));
+    }
+}
