4e2051e634bad9af9c7686b265d61fa138834384
clevdev <gusev.dev@gmail.com>
2019-05-13 22:43:11 +0300

    HW6. final

---
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -0,0 +1,60 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.*;
+
+public class MapResumeStorage extends AbstractStorage {
+
+    private Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected boolean isMember(Object key) {
+        return key != null;
+    }
+
+    @Override
+    protected Object getKeyOfElement(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected void saveElement(Resume resume, Object key) {
+        storage.put(((Resume) key).getUuid(), resume);
+
+    }
+
+    @Override
+    protected Resume getElementByKey(Object key) {
+        return (Resume)key;
+    }
+
+    @Override
+    protected void updateElement(Resume resume, Object key) {
+        saveElement(resume, ((Resume)key).getUuid());
+
+    }
+
+    @Override
+    protected void deleteElementByKey(Object key) {
+        storage.remove(((Resume) key).getUuid());
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        List<Resume> sortedlist = new ArrayList<Resume>(storage.values());
+        Collections.sort(sortedlist, RESUME_COMPARATOR);
+        return sortedlist;
+
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
